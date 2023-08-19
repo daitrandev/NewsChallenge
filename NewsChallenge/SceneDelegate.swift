fe193a1652302c5f3c145f29ff5664bfd5fd815c
DaiTran <daitran@vulcanlabs.co>
2023-08-19 10:52:44 +0700

    compose AllFeedsScene in SceneDelegate

--- NewsChallenge/SceneDelegate.swift
+++ NewsChallenge/SceneDelegate.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    private var navigationController: UINavigationController?
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let remoteFeedLoader = RemoteFeedLoader(client: URLSessionHTTPClient())
+        let localFeedLoader = LocalFeedLoader()
+        let loader = RemoteFeedLoaderWithLocalFallback(remoteLoader: remoteFeedLoader, localLoader: localFeedLoader)
+        let dataSource = AllFeedsSceneDataSource(items: [], didTapItem: { _ in })
+        let allFeedsScene = AllFeedsScene(vm: AllFeedsViewModel(loader: loader), dataSource: dataSource)
+        navigationController = UINavigationController(rootViewController: allFeedsScene)
+        window?.rootViewController = navigationController
     }
 }
 
